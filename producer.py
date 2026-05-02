441459ca47dc2ae0e752a1a5723611aa0da91b31
CHAPAPPA <frajchap@gmail.com>
2026-05-02 01:56:44 +0100

    producer.py  →  Kafka (iot-sensors)  →  Spark Streaming  →  Alert console working

--- producer.py
+++ producer.py
@@ -2,6 +2,7 @@ from kafka import KafkaProducer
 import json
 import random
 import time
+from datetime import datetime, timezone
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
@@ -13,11 +14,10 @@ while True:
         "sensor_id": random.randint(1, 5),
         "pm25": round(random.uniform(5, 60), 2),
         "no2": round(random.uniform(10, 80), 2),
-        "timestamp": time.time()
+        "timestamp": datetime.now(timezone.utc).isoformat()  # "2026-05-02T00:05:52+00:00"
     }
 
     producer.send("iot-sensors", data)
     print("Sent:", data)
 
     time.sleep(1)
-
--- stream.py
+++ stream.py
@@ -1,41 +1,45 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
+from pyspark.sql.functions import from_json, col, when
+from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, TimestampType
 
 spark = SparkSession.builder \
     .appName("AirQualityStreaming") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
     .getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
 
 schema = StructType([
     StructField("sensor_id", IntegerType()),
     StructField("pm25", DoubleType()),
     StructField("no2", DoubleType()),
-    StructField("timestamp", DoubleType())
+    StructField("timestamp", TimestampType())
 ])
 
-# READ FROM KAFKA
 df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:29092") \
     .option("subscribe", "iot-sensors") \
+    .option("startingOffsets", "latest") \
     .load()
 
 json_df = df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), schema).alias("data")) \
     .select("data.*")
 
-# ALERT RULES
 alerts = json_df.filter(
     (col("pm25") > 25) | (col("no2") > 40)
 ).withColumn(
     "alert",
-    when(col("pm25") > 25, "PM2.5 HIGH").otherwise("NO2 HIGH")
+    when(col("pm25") > 25, "PM2.5 HIGH")
+    .when(col("no2") > 40, "NO2 HIGH")
+    .otherwise("UNKNOWN")
 )
 
-# OUTPUT TO CONSOLE (TEST FIRST)
 query = alerts.writeStream \
     .outputMode("append") \
     .format("console") \
+    .option("truncate", False) \
     .start()
 
 query.awaitTermination()
