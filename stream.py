4e6c161b0bc1a3accd41dce70eaf74494f092b7b
ela-sarhani <ela.sarhaniii@gmail.com>
2026-05-03 02:42:07 +0100

    Add HDFS Paquet sink for Spark streaming output

--- stream.py
+++ stream.py
@@ -2,10 +2,16 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
+# ---------------------------
+# Spark Session
+# ---------------------------
 spark = SparkSession.builder \
-    .appName("KafkaToPostgres") \
+    .appName("KafkaToPostgresAndHDFS") \
     .getOrCreate()
 
+# ---------------------------
+# Schema
+# ---------------------------
 schema = StructType([
     StructField("sensor_id", IntegerType()),
     StructField("pm25", FloatType()),
@@ -13,6 +19,9 @@ schema = StructType([
     StructField("timestamp", StringType())
 ])
 
+# ---------------------------
+# Kafka Stream
+# ---------------------------
 df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:29092") \
@@ -25,12 +34,22 @@ parsed_df = json_df.select(
     from_json(col("json"), schema).alias("data")
 ).select("data.*")
 
+# Convert timestamp
 parsed_df = parsed_df.withColumn(
     "timestamp",
     to_timestamp(col("timestamp"))
 )
-def write_to_postgres(batch_df, batch_id):
 
+# ---------------------------
+# Output Function
+# ---------------------------
+def write_to_postgres_and_hdfs(batch_df, batch_id):
+
+    batch_df.cache()
+
+    # -----------------------
+    # PostgreSQL sink
+    # -----------------------
     batch_df.write \
         .format("jdbc") \
         .option("url", "jdbc:postgresql://postgres:5432/airquality") \
@@ -41,8 +60,25 @@ def write_to_postgres(batch_df, batch_id):
         .mode("append") \
         .save()
 
+    # -----------------------
+    # HDFS sink (Parquet)
+    # -----------------------
+    hdfs_path = "hdfs://namenode:9000/data/airquality"
+
+    batch_df.write \
+        .mode("append") \
+        .format("parquet") \
+        .save(hdfs_path)
+
+    batch_df.unpersist()
+
+# ---------------------------
+# Streaming Query
+# ---------------------------
 query = parsed_df.writeStream \
-    .foreachBatch(write_to_postgres) \
+    .foreachBatch(write_to_postgres_and_hdfs) \
+    .option("checkpointLocation", "hdfs://namenode:9000/checkpoints/airquality_pipeline") \
+    .trigger(processingTime="5 seconds") \
     .outputMode("append") \
     .start()
 
